a1113e6eaf7e96545f2965a987000ba17c8d3a6b
lchsk <mjlechowski@gmail.com>
2016-04-17 12:02:11 +0100

    Changed C-a etc

--- init.el
+++ init.el
@@ -162,10 +162,37 @@
 
 (setq org-replace-disputed-keys t)
 
-(global-set-key (kbd "C-c j") 'windmove-left)
-(global-set-key (kbd "C-c l") 'windmove-right)
-(global-set-key (kbd "C-c i") 'windmove-up)
-(global-set-key (kbd "C-c k") 'windmove-down)
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line]
+                'smarter-move-beginning-of-line)
+
+;;(global-set-key (kbd "C-c j") 'windmove-left)
+;;(global-set-key (kbd "C-c l") 'windmove-right)
+;;(global-set-key (kbd "C-c i") 'windmove-up)
+;;(global-set-key (kbd "C-c k") 'windmove-down)
 
 (require-package 'monokai-theme)
 ;;(load-theme 'monokai t)
@@ -183,8 +210,12 @@
 (setq rainbow-delimiters t)
 (require-package 'web-mode)
 (require-package 'ace-window)
-(global-set-key (kbd "M-h") 'ace-window)
+(global-set-key (kbd "M--") 'ace-window)
 (setq fci-rule-color "white")
+
+(global-set-key (kbd "C-x w") 'whitespace-mode)
+(global-set-key (kbd "M-[") 'previous-buffer)
+(global-set-key (kbd "M-]") 'next-buffer)
 ;; -----
 
 ;;----------------------------------------------------------------------------
