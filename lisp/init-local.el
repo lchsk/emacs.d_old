9c4549e5e07811f751b419640cc85c81263e6704
Maciej Lechowski <maciej.lechowski@geneity.co.uk>
2016-05-27 14:37:48 +0100

    Added kill-other-buffers

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -90,26 +90,38 @@ point reaches the beginning or end of the buffer, stop there."
   (next-line 1)
   (yank))
 
-  (defun copy-line (arg)
-    "Copy lines (as many as prefix argument) in the kill ring.
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
       Ease of use features:
       - Move to start of next line.
       - Appends the copy on sequential calls.
       - Use newline as last char even on the last line of the buffer.
       - If region is active, copy its lines."
-    (interactive "p")
-    (let ((beg (line-beginning-position))
-          (end (line-end-position arg)))
-      (when mark-active
-        (if (> (point) (mark))
-            (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
-          (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
-      (if (eq last-command 'copy-line)
-          (kill-append (buffer-substring beg end) (< end beg))
-        (kill-ring-save beg end)))
-    (kill-append "\n" nil)
-    (beginning-of-line (or (and arg (1+ arg)) 2))
-    (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+		(end (line-end-position arg)))
+	(when mark-active
+	  (if (> (point) (mark))
+		  (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+		(setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+	(if (eq last-command 'copy-line)
+		(kill-append (buffer-substring beg end) (< end beg))
+	  (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer
+      (delq (current-buffer)
+      (remove-if-not '(
+      lambda (x) (
+         or (buffer-file-name x)
+         (eq 'dired-mode
+        (buffer-local-value 'major-mode x))))
+        (buffer-list)))))
 
 ;; ---------------
 ;; End Functions
